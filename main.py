57f15b7d4e0e1efb78ef11e241813478c0000773
Suim <143478016+suim-park@users.noreply.github.com>
2023-11-20 22:29:19 +0000

    Update files

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 
+import pandas as pd
 import mlflow
 import mlflow.sklearn
 
 def main():
     # Load the dataset
-    df = pd.read_csv("/workspaces/IDS706-Mini-Project-12-sp699/penguins.csv")
+    penguins = pd.read_csv("/workspaces/IDS706-Mini-Project-12-sp699/penguins.csv")
     
-    le = LabelEncoder()
-    penguins['species'] = le.fit_transform(penguins['species'])
+    label_encoder = LabelEncoder()
+    penguins['island'] = label_encoder.fit_transform(penguins['island'])
+    penguins['sex'] = label_encoder.fit_transform(penguins['sex'])
+
+    print(penguins.dtypes)
 
     # Define your features and target variable
     X = penguins.drop('species', axis=1)
@@ -18,8 +22,15 @@ def main():
 
     # Split the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    
+    X_train.dropna(inplace=True)
+    y_train = y_train[X_train.index]
+
+    X_test.dropna(inplace=True)
+    y_test = y_test[X_test.index]
 
     model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
 
     with mlflow.start_run():
 
@@ -39,5 +50,5 @@ def main():
         # Log the model
         mlflow.sklearn.log_model(model, "model")
 
-if ____name__ == "__main__":
+if __name__ == "__main__":
     main()
